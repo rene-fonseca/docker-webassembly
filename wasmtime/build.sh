e011585f65464b42c92e4331f85e4fe0c620c242
René Fonseca <rene.m.fonseca@gmail.com>
2020-01-23 21:07:44 +0100

    Added automatic download of latest release.

--- wasmtime/build.sh
+++ wasmtime/build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-docker build --no-cache -t renefonseca/wasmtime:0.8.0 -t renefonseca/wasmtime:latest .
+TAG=`curl -s https://api.github.com/repos/bytecodealliance/wasmtime/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^\"]+)".*/\1/'`
+echo Building image for Wasmtime $TAG
+
+if [[ "$TAG" =~ ^(.*)([0-9]+\.[0-9]+\.[0-9]+)$ ]]; then
+  VERSION=${BASH_REMATCH[2]}
+else
+  echo "Unsupported version."
+  exit 1
+fi
+
+rm -rf wasmtime-*x86_64-linux*
+curl -s https://api.github.com/repos/bytecodealliance/wasmtime/releases/latest | grep "browser_download_url.*wasmtime-$TAG-x86_64-linux.tar.xz" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+tar xf wasmtime-$TAG-x86_64-linux.tar.xz
+mv wasmtime-$TAG-x86_64-linux wasmtime-x86_64-linux
+
+# --build-arg TAG=$TAG
+docker build --no-cache -t renefonseca/wasmtime:$VERSION -t renefonseca/wasmtime:latest .
